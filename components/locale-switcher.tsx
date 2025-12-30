7850dee39bfe2396c4a43b0abcb9df0d5d33ecae
Betsyg Richards <richardsbetsyg@gmail.com>
2025-12-30 16:53:40 +0800

    feat(i18n): 引入国际化

---
+++ components/locale-switcher.tsx
@@ -0,0 +1,30 @@
+// https://github.com/vercel/next.js/blob/canary/examples/i18n-routing-pages
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function LocaleSwitcher() {
+  const router = useRouter();
+  const { locales, locale: activeLocale } = router;
+
+  const otherLocales = (locales || []).filter(
+    (locale) => locale !== activeLocale,
+  );
+
+  return (
+    <div>
+      <p>Locale switcher:</p>
+      <ul>
+        {otherLocales.map((locale) => {
+          const { pathname, query, asPath } = router;
+          return (
+            <li key={locale}>
+              <Link href={{ pathname, query }} as={asPath} locale={locale}>
+                {locale}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
