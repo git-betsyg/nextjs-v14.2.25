28149ba4cc51c0616647e21600383ffab88058a8
Betsyg Richards <richardsbetsyg@gmail.com>
2025-12-31 15:13:44 +0800

    feat(module): 引入React-Query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,20 +4,32 @@ import { SessionProvider } from "next-auth/react";
 import Layout from "@/components/layout";
 import WebVitals from "@/components/web-vitals";
 import { Provider as JotaiProvider } from "jotai";
+import {
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import React from "react";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient());
+
   return (
     <>
       <WebVitals />
       <JotaiProvider>
-        <SessionProvider session={session}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </SessionProvider>
+        <QueryClientProvider client={queryClient}>
+          <HydrationBoundary state={pageProps.dehydratedState}>
+            <SessionProvider session={session}>
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            </SessionProvider>
+          </HydrationBoundary>
+        </QueryClientProvider>
       </JotaiProvider>
     </>
   );
