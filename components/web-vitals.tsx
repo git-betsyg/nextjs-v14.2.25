66fdaf4f0806bb4e164008e327b1328d3b7aa253
Betsyg Richards <richardsbetsyg@gmail.com>
2025-12-31 10:27:03 +0800

    feat(module): 引入Analytics

---
+++ components/web-vitals.tsx
@@ -0,0 +1,9 @@
+import { useReportWebVitals } from "next/web-vitals";
+
+export default function WebVitals() {
+  useReportWebVitals((metric) => {
+    console.log(metric);
+  });
+
+  return <></>;
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,20 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import Layout from "../components/layout";
+import WebVitals from "../components/web-vitals";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
+    <>
+      <WebVitals />
+      <SessionProvider session={session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </>
   );
 }
